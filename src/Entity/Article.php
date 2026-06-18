2f1f4f4553769fd26d987b15e0419c4920e094dd
Volbix <44008405+Volbix@users.noreply.github.com>
2026-06-18 09:09:56 +0200

    feat : securité

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -7,6 +7,11 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use App\Repository\ArticleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -17,6 +22,13 @@ use Symfony\Component\Serializer\Attribute\Groups;
 #[ApiResource(
     normalizationContext: ['groups' => ['article:read']],
     denormalizationContext: ['groups' => ['article:write']],
+    operations: [
+        new GetCollection(),
+        new Get(),
+        new Post(security: 'is_granted("ROLE_ADMIN")'),
+        new Patch(security: 'is_granted("ROLE_ADMIN")'),
+        new Delete(security: 'is_granted("ROLE_ADMIN")'),
+    ],
 )]
 #[ApiFilter(SearchFilter::class, properties: ['slug' => 'exact'])]
 #[ApiFilter(OrderFilter::class, properties: ['publishedAt', 'viewCount'])]
