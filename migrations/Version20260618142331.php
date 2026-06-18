a3bac6b0bac6ac85c4ea0ff276aed87b7b21b82a
Noé Garnier <Noe.GARNIER@etu.uca.fr>
2026-06-18 22:54:57 +0200

    CI

--- migrations/Version20260618142331.php
+++ migrations/Version20260618142331.php
@@ -22,7 +22,6 @@ final class Version20260618142331 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE reward (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, challenge_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_4ED1725398A21AC6 (challenge_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('ALTER TABLE reward ADD CONSTRAINT FK_4ED1725398A21AC6 FOREIGN KEY (challenge_id) REFERENCES challenge (id)');
-        $this->addSql('ALTER TABLE activity ADD CONSTRAINT FK_AC74095AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE bike CHANGE purchase_date purchase_date DATETIME DEFAULT NULL, CHANGE created_at created_at DATETIME NOT NULL, CHANGE retired_at retired_at DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE bike ADD CONSTRAINT FK_4CBC37807E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE challenge_participation ADD CONSTRAINT FK_223360DCA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
@@ -40,7 +39,6 @@ final class Version20260618142331 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE reward DROP FOREIGN KEY FK_4ED1725398A21AC6');
         $this->addSql('DROP TABLE reward');
-        $this->addSql('ALTER TABLE activity DROP FOREIGN KEY FK_AC74095AA76ED395');
         $this->addSql('ALTER TABLE bike DROP FOREIGN KEY FK_4CBC37807E3C61F9');
         $this->addSql('ALTER TABLE bike CHANGE purchase_date purchase_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE created_at created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE retired_at retired_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
         $this->addSql('ALTER TABLE challenge_participation DROP FOREIGN KEY FK_223360DCA76ED395');
