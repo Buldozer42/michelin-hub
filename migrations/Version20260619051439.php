3b9c4f3084f05b8f03d0f15c078f729c618c5025
Noé Garnier <Noe.GARNIER@etu.uca.fr>
2026-06-19 08:34:49 +0200

    CI

--- migrations/Version20260619051439.php
+++ migrations/Version20260619051439.php
@@ -29,17 +29,6 @@ final class Version20260619051439 extends AbstractMigration
         $this->addSql('ALTER TABLE challenge ADD prerequisite_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE challenge ADD CONSTRAINT FK_D7098951276AF86B FOREIGN KEY (prerequisite_id) REFERENCES challenge (id) ON DELETE SET NULL');
         $this->addSql('CREATE INDEX IDX_D7098951276AF86B ON challenge (prerequisite_id)');
-        $this->addSql('ALTER TABLE challenge_participation ADD CONSTRAINT FK_223360DCA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE challenge_participation ADD CONSTRAINT FK_223360DC98A21AC6 FOREIGN KEY (challenge_id) REFERENCES challenge (id)');
-        $this->addSql('ALTER TABLE objective ADD CONSTRAINT FK_B996F10198A21AC6 FOREIGN KEY (challenge_id) REFERENCES challenge (id)');
-        $this->addSql('ALTER TABLE reward ADD CONSTRAINT FK_4ED1725398A21AC6 FOREIGN KEY (challenge_id) REFERENCES challenge (id)');
-        $this->addSql('ALTER TABLE strava_account ADD CONSTRAINT FK_B8C0A750A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE tire ADD CONSTRAINT FK_A2CE96DBCF590E41 FOREIGN KEY (tire_line_id) REFERENCES tire_line (id)');
-        $this->addSql('ALTER TABLE tire ADD CONSTRAINT FK_A2CE96DBC54DC202 FOREIGN KEY (gum_id) REFERENCES gum (id)');
-        $this->addSql('ALTER TABLE tire_rating ADD CONSTRAINT FK_F206E474BC5ADD68 FOREIGN KEY (tire_id) REFERENCES tire (id)');
-        $this->addSql('ALTER TABLE user_tire ADD CONSTRAINT FK_D84FDA12D5A4816F FOREIGN KEY (bike_id) REFERENCES bike (id)');
-        $this->addSql('ALTER TABLE user_tire ADD CONSTRAINT FK_D84FDA12E0FEC426 FOREIGN KEY (tire_model_id) REFERENCES tire (id)');
-        $this->addSql('ALTER TABLE user_tire ADD CONSTRAINT FK_D84FDA127E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema): void
@@ -54,16 +43,5 @@ final class Version20260619051439 extends AbstractMigration
         $this->addSql('ALTER TABLE challenge DROP FOREIGN KEY FK_D7098951276AF86B');
         $this->addSql('DROP INDEX IDX_D7098951276AF86B ON challenge');
         $this->addSql('ALTER TABLE challenge DROP prerequisite_id');
-        $this->addSql('ALTER TABLE challenge_participation DROP FOREIGN KEY FK_223360DCA76ED395');
-        $this->addSql('ALTER TABLE challenge_participation DROP FOREIGN KEY FK_223360DC98A21AC6');
-        $this->addSql('ALTER TABLE objective DROP FOREIGN KEY FK_B996F10198A21AC6');
-        $this->addSql('ALTER TABLE reward DROP FOREIGN KEY FK_4ED1725398A21AC6');
-        $this->addSql('ALTER TABLE strava_account DROP FOREIGN KEY FK_B8C0A750A76ED395');
-        $this->addSql('ALTER TABLE tire DROP FOREIGN KEY FK_A2CE96DBCF590E41');
-        $this->addSql('ALTER TABLE tire DROP FOREIGN KEY FK_A2CE96DBC54DC202');
-        $this->addSql('ALTER TABLE tire_rating DROP FOREIGN KEY FK_F206E474BC5ADD68');
-        $this->addSql('ALTER TABLE user_tire DROP FOREIGN KEY FK_D84FDA12D5A4816F');
-        $this->addSql('ALTER TABLE user_tire DROP FOREIGN KEY FK_D84FDA12E0FEC426');
-        $this->addSql('ALTER TABLE user_tire DROP FOREIGN KEY FK_D84FDA127E3C61F9');
     }
 }
